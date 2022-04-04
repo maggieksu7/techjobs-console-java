71146b9767b2744beb2f4a6660be77465c2e10fe
Maggie Dotel <maggieksu7@gmail.com>
2022-04-03 23:04:38 -0500

    initial commit. Mostly Done, still revising

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -76,11 +76,37 @@ public class JobData {
 
             String aValue = row.get(column);
 
-            if (aValue.contains(value)) {
+            if (aValue.toLowerCase().contains(value.toLowerCase())) {
                 jobs.add(row);
             }
         }
 
+        return jobs;
+    }
+
+
+    //Search all columns for the given term
+
+    public static ArrayList<HashMap<String, String>> findByValue(String value) {
+        //load data, if not already loaded
+        loadData();
+
+        //TODO- implement this method
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+
+        for (HashMap<String, String> row : allJobs) {
+            for (String key : row.keySet()) {
+
+                String aValue = row.get(key);
+
+                if (aValue.toLowerCase().contains(value.toLowerCase())) {
+
+                    jobs.add(row);
+
+                    break;
+                }
+            }
+        }
         return jobs;
     }
 
--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -2,6 +2,7 @@ package org.launchcode.techjobs.console;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -111,6 +112,22 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
+        if (someJobs.size() ==0) {
+            System.out.print("No Results");
+            return;
+        }
+
+            for (HashMap<String, String> job:someJobs) {
+                String asterisks = "\n*****\n";
+
+                for (Map.Entry<String, String> jobColumn : job.entrySet()) {
+                    asterisks += (jobColumn.getKey() + ": " + jobColumn.getValue()+ "\n");
+                }
+
+                asterisks += "*****";
+                System.out.println(asterisks);
+            }
+
+       // System.out.println("printJobs is not implemented yet");
     }
 }
